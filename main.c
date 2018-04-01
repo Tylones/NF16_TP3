5ae352b2dade24e09697882a3711f6fe81ea5ca2
Unknown <etiennereitz@gmail.com>
2018-04-01 20:02:57 +0200

    Essai de quelques fonctions dans le main

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+#include "tp3.h"
 int main()
 {
+	BlockChain bc;
+	ajouterBlock(bc);
+	crediter(0,10,"Créditer de 10e",bc);
+	payer(0, -3.25, "Repas RU", bc);
 	printf("Hello World!\n");
 	return 0;
 }
